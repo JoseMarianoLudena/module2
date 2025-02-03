4b38a7ed953bcc4b4f976464fceab13a5f5d6b34
Jose Mariano <josem@example.com>
2025-02-03 22:57:09 +0100

    module2solution

---
+++ app.js
@@ -0,0 +1,52 @@
+(function () {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuy = this;
+        toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+        toBuy.buyItem = function (index) {
+            ShoppingListCheckOffService.buyItem(index);
+        };
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var bought = this;
+        bought.items = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        var toBuyItems = [
+            { name: "Apples", quantity: 5 },
+            { name: "Bananas", quantity: 3 },
+            { name: "Carrots", quantity: 4 },
+            { name: "Detergent", quantity: 1 },
+            { name: "Eggs", quantity: 12 }
+        ];
+
+        var boughtItems = [];
+
+        service.getToBuyItems = function () {
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function () {
+            return boughtItems;
+        };
+
+        service.buyItem = function (index) {
+            var item = toBuyItems.splice(index, 1)[0];
+            boughtItems.push(item);
+        };
+    }
+
+})();
